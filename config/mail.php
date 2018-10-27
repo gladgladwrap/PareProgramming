b27f76931e50404241f623a8bfb88b929e85a9e8
gladgladwrap <dwcooper92@gmail.com>
2018-10-26 22:34:46 -0300

    Add proper mail configuration.

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'testing@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME')
     ],
 
     /*
